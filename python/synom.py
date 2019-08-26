82eeaa1179c014ed166a6db495753eec4ef895fb
Ondřej Měkota <ondra@Ondrejs-MacBook-Pro.local>
2019-08-26 15:50:48 +0200

    in progress

---
+++ python/synom.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import vim, urllib, urllib.request, json
+
+def synom():
+    headers = {}
+
+    word = vim.eval("expand(\"<cword>\")")
+    urllib.request.
+    print(word+"Ahoj")
+
+def _get_current_word():
+    pass
+def _get_synoms():
+    pass
+def synoms():
+    pass
