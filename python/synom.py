28b8de223598c4380587d081ff10e3c378c3a3a4
Ondřej Měkota <ondra@Ondrejs-MacBook-Pro.local>
2019-08-26 17:28:31 +0200

    import upon call, slightly faster start

--- python/synom.py
+++ python/synom.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-import vim, requests, json, os
-
-def _get_my_key():
-    plugin_root_dir = vim.eval('s:plugin_root_dir')
-    file = os.path.normpath(os.path.join(plugin_root_dir, '..', 'data', 'my-key'))
-    with open(file, 'r') as f:
-        key = f.read()
-    return str(key.strip())
-
-def _get_current_word():
-    return vim.eval("expand(\"<cword>\")")
-
-def _get_synoms(word):
-    headers = {}
-    headers['x-rapidapi-host'] = 'wordsapiv1.p.rapidapi.com'
-    headers['x-rapidapi-key'] = _get_my_key()
-    url = 'https://wordsapiv1.p.rapidapi.com/words/{}/synonyms'.format(word)
-    with requests.request('GET', url, headers=headers) as resp:
-        data = resp.text
-    obj = json.loads(data)
-    return ', '.join(obj['synonyms'])
 
 def synoms():
+    import vim, requests, json, os
+    def _get_my_key():
+        plugin_root_dir = vim.eval('s:plugin_root_dir')
+        file = os.path.normpath(os.path.join(plugin_root_dir, '..', 'data', 'my-key'))
+        with open(file, 'r') as f:
+            key = f.read()
+        return str(key.strip())
+
+    def _get_current_word():
+        return vim.eval("expand(\"<cword>\")")
+
+    def _get_synoms(word):
+        headers = {}
+        headers['x-rapidapi-host'] = 'wordsapiv1.p.rapidapi.com'
+        headers['x-rapidapi-key'] = _get_my_key()
+        url = 'https://wordsapiv1.p.rapidapi.com/words/{}/synonyms'.format(word)
+        with requests.request('GET', url, headers=headers) as resp:
+            data = resp.text
+        obj = json.loads(data)
+        return ', '.join(obj['synonyms'])
+
     print(_get_synoms(_get_current_word()))
