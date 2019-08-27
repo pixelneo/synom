9654ea3eda977192296b7cb01891ef98f132a898
Ondřej Měkota <ondrej.mekota@me.com>
2019-08-27 15:22:32 +0200

    making custom dict

--- python/synom.py
+++ python/synom.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def synoms():
-    import vim, requests, json, os
+    import vim, requests, json, os, builtins
+    class mydict(dict):
+        pass
     def _get_my_key():
         plugin_root_dir = vim.eval('s:plugin_root_dir')
         file = os.path.normpath(os.path.join(plugin_root_dir, '..', 'data', 'my-key'))
@@ -16,21 +18,52 @@ def synoms():
     def _get_current_word():
         return vim.eval("expand(\"<cword>\")")
 
-    def _get_synoms(word):
+    def _get_data_from_server(word, what=''):
         headers = {}
         headers['x-rapidapi-host'] = 'wordsapiv1.p.rapidapi.com'
         headers['x-rapidapi-key'] = _get_my_key()
-        url = 'https://wordsapiv1.p.rapidapi.com/words/{}/synonyms'.format(word)
+        url = 'https://wordsapiv1.p.rapidapi.com/words/{}/{}'.format(word, what)
         try:
             with requests.request('GET', url, headers=headers) as resp:
                 data = resp.text
         except requests.exceptions.RequestException as e:
-            print('Synom ERROR: Error with retriving data from Words API server')
+            print('Synom ERROR: Error occured when retrieving data from Words API server')
             exit()
         obj = json.loads(data)
+        return obj 
+
+    def _get_synoms(word):
+        obj = _get_data_from_server(word, 'synonyms')
         if obj is not None and isinstance(obj, dict) and 'synonyms' in obj.keys():
             return ', '.join(obj['synonyms'])
         else:
             return '--- No synonyms ---'
 
-    print(_get_synoms(_get_current_word()))
+    def _get_it_all(word):
+        def set_value(d, d2, k, k2):
+            if k2 in d2:
+                d[k] = d2
+
+        out_list = {}
+        obj = _get_data_from_server(word)
+        if obj is not None and isinstance(obj, dict) and isinstance(obj['results'], list):
+
+            out_list['word'] = obj['word']      # add searched word
+            out_list['meanings'] = []
+
+            for meaning in obj['results']:
+                x = {}
+                x['def'] = meaning['definition']
+                x['s
+                out_list['meanings'].append(
+
+
+
+
+        else:
+            return '--- Error when parsin response ---'
+            
+
+    # print(_get_synoms(_get_current_word()))
+    vim.command("set buftype=nofile")
+    vim.command("pedit! /tmp/Synom-temp-file")
